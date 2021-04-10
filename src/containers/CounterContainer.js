142e8ac0b514a721e181cce28c028e06e0132568
unknown <sonwj0915@naver.com>
2021-04-10 17:10:25 +0900

    useSelector 최적화

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,14 +1,28 @@
 import React from "react";
 import Counter from "../components/Counter";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch, shallowEqual } from "react-redux";
 import { decrease, increase, setDiff } from "../modules/counter";
 
 // state는 redux의 현재 상태
 function CounterContainer() {
-  const { number, diff } = useSelector((state) => ({
-    number: state.counter.number,
-    diff: state.counter.diff,
-  }));
+  // 이렇게 하면 Counter도 자꾸 리렌더링 시킴
+  // const { number, diff } = useSelector((state) => ({
+  //   number: state.counter.number,
+  //   diff: state.counter.diff,
+  // }));
+
+  // 대안 1 (따로 따로 여러번 불러와 준다)
+  // const number = useSelector((state) => state.counter.number);
+  // const diff = useSelector((state) => state.counter.diff);
+
+  // 대안 2 (shallowEqual 추가)얕게 비교함
+  const { number, diff } = useSelector(
+    (state) => ({
+      number: state.counter.number,
+      diff: state.counter.diff,
+    }),
+    shallowEqual
+  );
 
   const dispatch = useDispatch();
 
